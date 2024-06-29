81c69b86c9d4403d3cb2f7d1c883ae54466e472f
Piyush <02jan06@gmail.com>
2024-06-29 21:36:34 +0530

    added shimmer component and removed mock data , fetching data from api

--- Readme.md
+++ Readme.md
@@ -18,3 +18,7 @@
 - react re-render by comparing Two js Objects without messing with HTML(real DOM) it compares Js Objects(Virtual DOM- React components)
 
 - Virtual DOM is the representation of a actual DOM => normal javascript Object like "<Body />
+
+- Best Practice LOAD -> RENDER -> API/Functions -> RENDER
+
+- Use shimmer ui (fake cards when data is not fetched for render)
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
-import resObj from "../utils/mockdata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body=()=>{
 
     // Local state variable- Super powerful variable;
-    let [ListofRestaurant,setListofRestaurant]=useState(resObj);
+    let [ListofRestaurant,setListofRestaurant]=useState([]);
+
+    // USE Effect - ITS 2 arguments callback and an dependency list
+    // Its callback function call after components rendered
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    
+    // Using Swiggy APi to get data (USE CORS Extension)
+    const fetchData=async()=>{
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.66500&lng=77.44770&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json= await data.json();
+        console.log(json);
+        // Optional Chaining 
+        setListofRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
 
     //Filter Button 
     const FilterButton=()=>(
@@ -24,7 +41,17 @@ const Body=()=>{
                     }
             }}>All Restaurants</button>
 
+    
+    // can be used to Show loading screen to user until data is not fetching 
+    // if(ListofRestaurant === 0){
+    //     // Loader animation
+    // }
 
+    
+    // Condintional Rendering
+    if(ListofRestaurant == 0){
+       return <Shimmer/>;
+    }
     return(
         <div className="body">
             <div className="filter">
